5a4c16d33e5bc0bc818c2f7031bb4177df64cee4
aetonsi <18366087+aetonsi@users.noreply.github.com>
2023-01-16 12:27:04 +0100

    Add project files.

---
+++ printer/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// https://stackoverflow.com/questions/199528/c-sharp-console-receive-input-with-pipe
+string text;
+if (Console.IsInputRedirected) {
+    // Console.Error.WriteLine("pipe");
+    text = new StreamReader(Console.OpenStandardInput()).ReadToEnd();
+} else {
+    // Console.Error.WriteLine("args");
+    text = String.Join(' ', Environment.GetCommandLineArgs().Skip(1).ToArray());
+}
+
+Console.Write(text);
