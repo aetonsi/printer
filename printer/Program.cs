f5a324e78644066a7a4438e4d6534ab7a8928abf
aetonsi <18366087+aetonsi@users.noreply.github.com>
2023-01-27 15:15:15 +0100

    command line arguments override piped data

--- printer/Program.cs
+++ printer/Program.cs
@@ -1,11 +1,14 @@
 ﻿
 // https://stackoverflow.com/questions/199528/c-sharp-console-receive-input-with-pipe
-string text;
-if (Console.IsInputRedirected) {
-    // Console.Error.WriteLine("pipe");
+string text = "";
+
+// piping AND no command line arguments (arg 0 is the program itself)
+if (Console.IsInputRedirected && Environment.GetCommandLineArgs().Length == 1)
+{
     text = new StreamReader(Console.OpenStandardInput()).ReadToEnd();
-} else {
-    // Console.Error.WriteLine("args");
+}
+else if (Environment.GetCommandLineArgs().Length > 1) // if at least 1 command line argument (arg 0 is the program itself)
+{
     text = String.Join(' ', Environment.GetCommandLineArgs().Skip(1).ToArray());
 }
 
