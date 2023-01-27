b515a3b693b58497b9588cd3f504a62c513c356b
aetonsi <18366087+aetonsi@users.noreply.github.com>
2023-01-27 16:26:24 +0100

    test sign in vs

--- printer/Program.cs
+++ printer/Program.cs
@@ -1,6 +1,5 @@
 ﻿
 // https://stackoverflow.com/questions/199528/c-sharp-console-receive-input-with-pipe
-
 string text = "";
 
 // piping AND no command line arguments (arg 0 is the program itself)
